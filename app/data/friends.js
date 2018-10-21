fedb91bbb36449a208f99d3e984c9f96d3d4f67a
Carlos Candia <carloscandia@MacBook-Pro-2.attlocal.net>
2018-10-21 11:12:05 -0500

    Working App

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Alicia Vikander',
+		photo: 'https://media.wmagazine.com/photos/5853e929e3d613c03e1edb6a/4:3/w_1536/alicia-vikander-cover.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Gal Gadot',
+		photo: 'https://pbs.twimg.com/media/DmgXZ4UW0AETas6.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Melissa McCarthy',
+		photo: 'https://media.npr.org/assets/img/2017/02/12/melissamccarthy_wide-da866c3b03608852d883a38fb4e715836dac0d63.png?s=1400',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Cate Blanchett',
+		photo: 'https://images.perthnow.com.au/publication/329FD417A9AC0A65D2DDAC62D953C481/0b14a386a8129373f25643198332a911.jpeg?imwidth=668&impolicy=pn_v1',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Jennifer Lawrence',
+		photo: 'https://lovelace-media.imgix.net/getty/498852520.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'George Clooney',
+		photo: 'http://i.imgur.com/HOZC9C5.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Dwayne Johnson',
+		photo: 'https://i.pinimg.com/originals/8a/06/1c/8a061c579f4c0c9946bfed2910df2379.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Bob Downey',
+		photo: 'https://i.pinimg.com/originals/b5/35/5e/b5355e20ca4e6c7938b8fb3d009bf304.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Scarlett Johansson',
+		photo: 'https://assets.vogue.com/photos/58dedeaaa6df677eb9f7df72/master/pass/00-holding-scarlett-johansson-5-things.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Jackie Chan',
+		photo: 'https://timedotcom.files.wordpress.com/2016/09/ap_120519112383.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
